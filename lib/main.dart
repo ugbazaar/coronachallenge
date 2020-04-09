8abef917ff0eaeda116cc57dddb49729c46d4c3c
Kiran Timsina <timsinakiran@gmail.com>
2020-04-09 16:03:18 +0545

    refactor of codes into widget and conditional result

--- lib/main.dart
+++ lib/main.dart
@@ -5,30 +5,17 @@ import './answer.dart';
 
 void main() => runApp(QuizApp());
 
-class QuizApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Corona Time Day 3!'),
-        ),
-        body: QuizCard(),
-      ),
-    );
-  }
-}
-
-class QuizCard extends StatefulWidget {
+class QuizApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _QuizCardState();
+    return _QuizAppState();
   }
 }
 
-class _QuizCardState extends State<QuizCard> {
-  final questions = [
+class _QuizAppState extends State<QuizApp> {
+  var _questionIndex = 0;
+
+  final questions = const [
     {
       'question': 'Name this frozen lake:',
       'options': ['Tilicho', 'Shey Phoksundo', 'It\'s not a lake!', 'Rara'],
@@ -64,23 +51,32 @@ class _QuizCardState extends State<QuizCard> {
           'https://static.ugbazaar.com/uploads/quiz/question/medium/53-name-this-popular-destination.jpeg'
     }
   ];
-  var _questionIndex = 0;
 
   void _answerQuestion() {
+    // _makeGetRequest();
     setState(() {
-      _questionIndex = (_questionIndex + 1) % questions.length;
+      _questionIndex = _questionIndex + 1;
     });
     print('Answered Question Number ' + (_questionIndex + 1).toString());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Question(_questionIndex, questions[_questionIndex]['question'],
-          questions[_questionIndex]['media']),
-      ...(questions[_questionIndex]['options'] as List<String>).map((option) {
-        return Answer(_answerQuestion, option);
-      }).toList(),
-    ]);
+    return MaterialApp(
+      title: 'Quiz App Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Corona Time Day 3!'),
+        ),
+        body: Column(children: [
+          Question(_questionIndex, questions[_questionIndex]['question'],
+              questions[_questionIndex]['media']),
+          ...(questions[_questionIndex]['options'] as List<String>)
+              .map((option) {
+            return Answer(_answerQuestion, option);
+          }).toList(),
+        ]),
+      ),
+    );
   }
 }
